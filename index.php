7ee5a83d91da5d98ef97f2409b0c52d8e9c5871d
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-01-07 14:44:06 +0100

    fixed redirection

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/setup/');
-	exit;
+// Redirect to ./setup/
+header("Location: ./setup/");
 ?>
 Something is wrong with the webserver
