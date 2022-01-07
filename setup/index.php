8a27ebddf698a40502bd4a2bfb647c0c1b3eebb2
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-01-07 14:43:07 +0100

    added starting page

---
+++ setup/index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomeUI - Setup</title>
+    <!-- Link the stylesheet setup_style.css -->
+    <link rel="stylesheet" href="setup_style.css">
+</head>
+<body>
+    <h1>HomeUI</h1>
+    <p>
+    Danke, dass du dich f&uuml;rs nutzen von HomeUI entschieden hast.
+    </p>
+    <p>
+    Dieses Setup-Tool wird dich durch ein paar einfache Schritte durchf&uuml;hren, um die Installation von HomeUI zu erm&ouml;glichen.
+    </p>
+    <p>	
+    Bitte wähle nun die Sprache die du nutzen m&ouml;chtest.
+    </p>
+    <img class="lang_flag" onclick="window.location.href='./de/1.html'" src="./icons/de.svg" alt="Deutsch">
+    <img class="lang_flag" onclick="window.location.href='./en/1.html'" src="./icons/en.svg" alt="English">
+</body>
+</html>
