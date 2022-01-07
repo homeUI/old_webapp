2ae15c54836fc8152b6264140b80e7599ceb78df
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-01-07 18:07:05 +0100

    added login functionality for database

--- login/login.php
+++ login/login.php
@@ -1,35 +1,56 @@
 <?php
 
+    // Database name
+    $dbname = "homeui";
+
     // Include the config file
-    include('config.php');
+    include('../config.php');
 
     // Start sessions
     session_start();
 
-    // Check if the user is logged in
-    // If so, redirect to ../index.php
-    if (isset($_SESSION['loggedin'])) {
-        header('Location: ../index.php');
-        exit;
+    // Connect to the database
+    $con = new mysqli($db_host, $db_user, $db_pass, $dbname);
+
+    // Check connection
+    if ($con->connect_error) {
+        exit("Connection failed: " . $con->connect_error);
     }
+    
+    // Get the form input 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
     // Check if the username and password are correct
-    if ($_POST['username'] == $username && $_POST['password'] == $password) {
+    if ($stmt = $con->prepare('SELECT password FROM users WHERE username = ?')) {
+        
+        // Bind parameters (s = string, i = int, b = blob, etc)
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        
+        // Store the result so we can check if the account exists in the database.
+        $stmt->store_result(); 
+        
+        // Check if Account exist
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($password);
+            $stmt->fetch();
 
-        // Set the loggedin session variable to true
-        $_SESSION['loggedin'] = true;
-
-        // Redirect to ../index.php
-        header('Location: ../index.php');
-        exit;
-
+            if (password_verify($_POST['password'], $password)) {                
+                session_regenerate_id();
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION['name'] = $_POST['username'];
+                
+                header('Location: ../app/');
+                exit;
+            } else {
+                exit("Wrong password");
+            }
+        } else {
+            exit("Wrong username");
+        }
     } else {
-
-        exit("<h1>Login failed</h1>");
-
+        exit("Error: " . $con->error);
     }
-
-
-    
     
 ?>
